f8ab1e3abb86780aa9545deb53f76941ed0260c8
zirosusilo <muhammadziro454@gmail.com>
2025-10-30 11:05:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Tombol ID
+const tombolSapa = document.getElementById('sapaButton');
+// Event listener
+tombolSapa.addEventListener('click', function() {
+  // Pemberitahuan saat tombol diklik
+  alert('Terima kasih sudah menyapa :)');
+});
